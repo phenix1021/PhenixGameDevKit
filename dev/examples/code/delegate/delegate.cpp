211fc77c61ebdfe7464563abaa325d135960097d
phenix102 <phenix1021@sina.co>
2015-04-23 20:24:58 +0800

    example

---
+++ dev/examples/code/delegate/delegate.cpp
@@ -0,0 +1,18 @@
+// delegate.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <Base/SimpleDelegate.h>
+//#include <Timer/Timer.h> 
+//#include <Memory/BinaryFrameAlloc.h>
+//#include <Memory/ObjectPool.h>
+//#include <Base/AutoList.h>
+#include <Base/Ref.h>
+#include <Base/Noncopyable.h>
+//#include <Base/Parser.h>
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	return 0;
+}
+
